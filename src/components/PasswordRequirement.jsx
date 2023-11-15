df00b9bdf40cee2f88e57723f0e17afa8c7d6d52
Parasuramandckap <parasuramanadckap@gmail.com>
2023-11-15 20:30:14 +0530

    change variable name to CamelCase

--- src/components/PasswordRequirement.jsx
+++ src/components/PasswordRequirement.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 const PasswordRequirements = ({ password }) => {
   // Define your password requirements here
-  const minLength = 8;
-  const UpperCase = /[A-Z]/.test(password);
-  const LowerCase = /[a-z]/.test(password);
-  const atLeastOneNumber = /\d/.test(password);
-  const SpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password);
+  const lengthCheck = 8;
+  const upperCaseCheck = /[A-Z]/.test(password);
+  const lowerCaseCheck = /[a-z]/.test(password);
+  const numberCheck = /\d/.test(password);
+  const specialCharCheck = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password);
 
   // Define styles for correct and wrong states
   const correctStyle = { color: "blue" };
@@ -13,51 +13,51 @@ const PasswordRequirements = ({ password }) => {
 
   return (
     <ul className="password-check-points">
-      <li style={password.length >= minLength ? correctStyle : wrongStyle}>
-        {password.length >= minLength && (
+      <li style={password.length >= lengthCheck ? correctStyle : wrongStyle}>
+        {password.length >= lengthCheck && (
           <span className="material-symbols-outlined">check_circle</span>
         )}
-        {password.length < minLength && (
+        {password.length < lengthCheck && (
           <span className="material-symbols-outlined">cancel</span>
         )}
         {`Minimum 8 characters`}
       </li>
 
-      <li style={UpperCase ? correctStyle : wrongStyle}>
-        {UpperCase && (
+      <li style={upperCaseCheck ? correctStyle : wrongStyle}>
+        {upperCaseCheck && (
           <span className="material-symbols-outlined">check_circle</span>
         )}
-        {!UpperCase && (
+        {!upperCaseCheck && (
           <span className="material-symbols-outlined">cancel</span>
         )}
         {`At least one uppercase letter`}
       </li>
 
-      <li style={LowerCase ? correctStyle : wrongStyle}>
-        {LowerCase && (
+      <li style={lowerCaseCheck ? correctStyle : wrongStyle}>
+        {lowerCaseCheck && (
           <span className="material-symbols-outlined">check_circle</span>
         )}
-        {!LowerCase && (
+        {!lowerCaseCheck && (
           <span className="material-symbols-outlined">cancel</span>
         )}
         {`At least one lowercase letter`}
       </li>
 
-      <li style={atLeastOneNumber ? correctStyle : wrongStyle}>
-        {atLeastOneNumber && (
+      <li style={numberCheck ? correctStyle : wrongStyle}>
+        {numberCheck && (
           <span className="material-symbols-outlined">check_circle</span>
         )}
-        {!atLeastOneNumber && (
+        {!numberCheck && (
           <span className="material-symbols-outlined">cancel</span>
         )}
         {`At least one digit`}
       </li>
 
-      <li style={SpecialChar ? correctStyle : wrongStyle}>
-        {SpecialChar && (
+      <li style={specialCharCheck ? correctStyle : wrongStyle}>
+        {specialCharCheck && (
           <span className="material-symbols-outlined">check_circle</span>
         )}
-        {!SpecialChar && (
+        {!specialCharCheck && (
           <span className="material-symbols-outlined">cancel</span>
         )}
         {`At least one special character`}

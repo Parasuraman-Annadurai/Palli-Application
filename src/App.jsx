b19bc440c47f5f1383e0b273e547bce924214b83
Parasuramandckap <parasuramanadckap@gmail.com>
2023-11-16 16:20:00 +0530

    remove unwanted screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginScreen from "./pages/login/LoginScreen";
 
-import React from "react";
+
 //Define your routes for APP here
 
 const App = () => {

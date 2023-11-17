8dd75c64c267ff28e33510277f67caeaac4d1be0
Parasuramandckap <parasuramanadckap@gmail.com>
2023-11-17 13:57:42 +0530

    dashboard route added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import ForgotPassword from "../src/pages/ForgotPage/ForgotScreen";
 import ChangePassword from "../src/pages/ChangePasswordPage/ResetPasswordScreen";
 import LoginScreen from "./pages/login/LoginScreen";
-
+import DasboardPage from "./pages/dashboard/DashboardPage";
 //Define your routes for APP here
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
       <Route path="/login" element={<LoginScreen />}></Route>
       <Route path="/forgot/password" element={<ForgotPassword />}></Route>
       <Route path="/change/password" element={<ChangePassword />}></Route>
+      <Route path="/dashboard" element={<DasboardPage />}></Route>
     </Routes>
   );
 };
---
+++ src/pages/dashboard/DashboardPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+const DasboardPage = () => {
+    return ( <div>welcome</div> );
+}
+ 
+export default DasboardPage;

92ae3ea02e497eb2c5b20fff49acc7cf24bed735
Parasuramandckap <parasuramanadckap@gmail.com>
2023-11-21 11:41:38 +0530

    admin dashboard added

---
+++ src/pages/adminDashoard/components/navbar.jsx
@@ -0,0 +1,15 @@
+// Navbar.js
+import React from 'react';
+import Profile from './profile';
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>
+        <h2>Welcome to Vignesh Selvaraj</h2>
+      </div>
+      <Profile/>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/adminDashoard/components/profile.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Popover, Button } from 'antd';
+
+
+const Profile = () => {
+  const content = (
+    <div>
+      <p>
+        <strong>Username</strong>
+        <br />
+        user@example.com
+      </p>
+      <Button type="link" >
+        Logout
+      </Button>
+    </div>
+  );
+  return (
+    <div className='avatar__container'>
+       <Popover content={content} placement="bottomRight" trigger="click">
+          <div className='avatar__logo'>
+           <span className="material-symbols-outlined">account_circle</span>
+          </div>
+        </Popover>
+    </div>
+  );
+};
+
+export default Profile;

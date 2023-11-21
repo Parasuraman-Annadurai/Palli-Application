917a388c1d47761a0a3ada1db1e5ee7fd26c0292
Ganeshdckap <ganeshmdckap@gmail.com>
2023-11-21 16:22:09 +0530

    forgot password page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import LoginScreen from "./pages/login/LoginScreen";
 import AdminDashoboard from './pages/adminDashoard/AdminDashboard';
 import ForgotPassword from "./pages/ForgotPage/ForgotPage";
-import ChangePassword from "./pages/ChangePasswordPage/ResetPasswordPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginScreen />}></Route>
       <Route path="/forgot/password" element={<ForgotPassword />}></Route>
-      <Route path="/change/password" element={<ChangePassword />}></Route>
       <Route path="/home" element={<AdminDashoboard />}></Route>
     </Routes>
   );

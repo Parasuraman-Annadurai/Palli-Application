489b655e7bb58a3a96a20d8cb153616aa9dc6120
avinashdckap <avinashs@dckap.com>
2023-11-27 12:38:36 +0530

    user added in context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,13 +3,17 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+
   const [token, setToken] = useState(() =>
     localStorage.getItem("token")
       ? JSON.parse(localStorage.getItem("token"))
       : {}
   );
 
-  const [user, setUser] = useState({username:"parasu"});
+  const [user, setUser] = useState(() =>
+  localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : {});
 
   return (
     <AuthContext.Provider value={{ user, token, setUser, setToken }}>
